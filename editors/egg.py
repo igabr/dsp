480709e0fb8c68987322ce1a85a445a91935569a
Ibrahim Gabr <igabr@uchicago.edu>
2017-06-15 22:31:57 -0500

    used text editors

---
+++ editors/egg.py
@@ -0,0 +1,3 @@
+print("Emacs seems to be quite intuitive!")
+print("This is way better than vim - seems to be on par with nano!")
+print("Then again, Im sure I just think that because im using basic commands")
---
+++ editors/vinegar.py
@@ -0,0 +1,3 @@
+print("just testing this out")
+print("Vim seems to be very complicated when compared to nano!")
+print("i learned how to delete swap files - that was cool")
